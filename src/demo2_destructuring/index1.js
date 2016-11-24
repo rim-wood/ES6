a1d67c3752e8b26a314b9d1f6d5d20e9527243e8
will-min <wlm195@icloud.com>
2016-11-24 20:53:12 +0800

    little edit demo2

--- src/demo2_destructuring/index1.js
+++ src/demo2_destructuring/index1.js
@@ -1,3 +1,4 @@
+import 'babel-polyfill';
 //解构赋值,参数与值一一对应就能被赋值
 let [a,b,c] = [1,2,3];
 let [d,[e,[f]]] = [1,[2,[3]]];
@@ -11,3 +12,15 @@ console.log(g);//undefined
 //不完全解构
 let [h,[i],j] = [1,[2,3],4];
 console.log(h+"--"+i+"--"+j);//1--2--4
+
+//事实上，只要具有Iterator接口，都可采用数组形式的解构赋值
+function* fibs(){
+  let a = 0;
+  let b = 1;
+  while (true){
+    yield a;
+    [a, b] = [b, a + b];
+  }
+}
+let [one, tow, three, four, fifw, six, seven, eight, nine] = fibs();
+console.log(seven); //8
